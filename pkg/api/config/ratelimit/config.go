c0ab7483a47c0c092ca97b76e1508b163ca7fa4f
mark4z <anymre@outlook.com>
2021-06-10 11:14:26 +0800

    ratelimit

--- pkg/api/config/ratelimit/config.go
+++ pkg/api/config/ratelimit/config.go
@@ -30,6 +30,7 @@ type Config struct {
 
 // Resource API group for rate limit, all API in group is considered to be the same resource
 type Resource struct {
+	ID    int64  `json:"id,omitempty" yaml:"id,omitempty"`
 	Name  string `json:"name" yaml:"name"`
 	Items []Item `json:"items" yaml:"items"`
 }
@@ -42,6 +43,7 @@ type Item struct {
 
 // Rule api group 's rate-limit rule
 type Rule struct {
+	ID        int64 `json:"id,omitempty" yaml:"id,omitempty"`
 	flow.Rule `yaml:",inline"`
 	Enable    bool `json:"enable" yaml:"enable"`
 }
