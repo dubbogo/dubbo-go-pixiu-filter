fa02f337f32816a6a9ddaf5bcf6440adcfb5d960
mark4z <anymre@outlook.com>
2021-08-01 14:02:30 +0800

    remove filter.go

--- pkg/context/context.go
+++ pkg/context/context.go
@@ -26,7 +26,7 @@ type Context interface {
 	Next()
 	Abort()
 	AbortWithError(string, error)
-	AppendFilterFunc(ff ...func(ctx Context))
+	AppendFilter(ff ...func(Context))
 
 	Status(code int)
 	StatusCode() int
