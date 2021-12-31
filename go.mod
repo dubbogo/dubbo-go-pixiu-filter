b56b3f9d88b13d3b494fbe6e6d6b93825cbd06b1
mazhihui <mazhihui@unipus.cn>
2021-12-31 09:30:20 +0800

    use google.golang.org/protobuf only

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module github.com/dubbogo/dubbo-go-pixiu-filter
 
 go 1.14
 
-require (
-	github.com/golang/protobuf v1.5.2
-	google.golang.org/protobuf v1.27.1
-)
+require google.golang.org/protobuf v1.27.1
--- pkg/xds/model/model.go
+++ pkg/xds/model/model.go
@@ -1,4 +1,4 @@
 package model
 
 //run to generate new model from ./proto/*.proto
-//go:generate protoc -I=./proto --go_out=./ ./proto/*.proto
+//go:generate protoc -I=.  --go_opt=Madapter.proto=./model --go_opt=Maddress.proto=./model --go_opt=Mbootstrap.proto=./model --go_opt=Mcluster.proto=./model --go_opt=Mextension.proto=./model --go_opt=Mfilter.proto=./model --go_opt=Mlistener.proto=./model --go_opt=Mroute.proto=./model --go_out=../../ ./*.proto
